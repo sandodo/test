0d11e93334f74e9c256daed627f153853fff8afa
sandodo <zouxiong@gmail.com>
2014-07-04 10:35:33 +0800

    Create 072.cpp
    add test source for checking usage of virtual functions in constructor and destructor

---
+++ 072.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+#include <boost/mpl/lambda.hpp>
+#include <boost/mpl/transform.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/for_each.hpp>
+
+class A
+{
+public:
+        A()
+        {
+                std::cout << "A()" << std::endl;
+                f();
+        }
+
+        ~A()
+        {
+                std::cout << "~A()" << std::endl;
+                f();
+        }
+
+        void cf()
+        {
+                std::cout << "A::cf()" << std::endl;
+                f();
+        }
+
+        virtual void f()
+        {
+                std::cout << "A::f()" << std::endl;
+        }
+
+};
+
+class B : public A
+{
+public:
+        B()
+        {
+                std::cout << "B()" << std::endl;
+        }
+
+        ~B()
+        {
+                std::cout << "~B()" << std::endl;
+        }
+
+        virtual void f()
+        {
+                std::cout << "B::f()" << std::endl;
+        }
+
+
+};
+
+int main()
+{
+
+        B bobj;
+
+        bobj.f();
+        bobj.cf();
+
+        return 0;
+}
+
